8bee8478178e445c94823acb2a6b20aac46f6feb
ArtiomLev <artiomlevshykov@gmail.com>
2026-04-16 00:25:35 +0300

    [chore]: remove deprecated code

--- src/led.cpp
+++ src/led.cpp
@@ -13,21 +13,5 @@ void led::setup() {
 }
 
 void led::tick() {
-    /*// Бегающая точка с затухающиим следом
-    static unsigned long effect_tmr = millis();
-    if (millis() - effect_tmr >= 50) {
-        FastLED.clear();
-        static int counter = 0;
-        if (counter > LEDS_NUM) counter = 0;
-        for (byte i = 0; i <= 6; i++) {
-            int led_num = counter + i;
-            if (led_num > LEDS_NUM) led_num = led_num - LEDS_NUM;
-            leds[counter + i].r = pow(2, i);
-            //            leds[counter+i].g = pow(2, i);
-            //            leds[counter+i].b = pow(2, i);
-        }
-        counter++;
-        FastLED.show();
-        effect_tmr = millis();
-    }*/
+    
 }
